9724f57ba35afa9b3f9b9599a36cfdc9827a84e3
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-02-04 17:28:36 +0100

    Add validation + config + logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configservice: ConfigService) => {
+        const isProduction = configservice.get('NODE_ENV') === 'production';
+        return {
+          pinoHttp: {
+            transport: isProduction
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                  options: { singleLine: true },
+                },
+            level: isProduction ? 'info' : 'debug',
+          },
+        };
+      },
+    }),
+    ConfigModule.forRoot(),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
