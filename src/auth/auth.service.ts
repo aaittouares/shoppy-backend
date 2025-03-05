df8a19895f03c1f5a5b1ffd42f1f11a98de91773
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-03-05 03:09:55 +0100

    Add login method to auth service : - sign JWT with user and create cookie Add custom decorator to grab user during req

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,48 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import ms from 'ms';
 import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import { TokenPayload } from './token-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getMilliseconds() +
+        ms(this.configService.getOrThrow<ms.StringValue>('JWT_EXPIRATION')),
+    );
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      expires,
+      secure: true,
+      httpOnly: true,
+    });
+
+    return { tokenPayload };
+  }
+
   async verifyUser(email: string, password: string) {
     try {
       const user = await this.userService.getUser({ email });
-      const authenticathed = await bcrypt.compare(password, user.password);
-      if (!authenticathed) throw new UnauthorizedException();
+      const authenticated = await bcrypt.compare(password, user.password);
+      if (!authenticated) throw new UnauthorizedException();
       return user;
     } catch (err) {
       throw new UnauthorizedException('Credentials are not valid.');
