5b11f4750b35af9a614cb9dff605c9d96a76587f
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-09-09 02:43:35 +0200

    Add verifyToken to Auth service and modify module to export the service Implements HandleConnection in ProductsGateway and use verifyToken from Auth service to authorize Ws connection based on jwt string in handshake

--- src/products/products.gateway.ts
+++ src/products/products.gateway.ts
@@ -1,12 +1,31 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import {
+  OnGatewayConnection,
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { AuthService } from '../auth/auth.service';
 
 @WebSocketGateway({ cors: { origin: '*' } })
-export class ProductsGateway {
+export class ProductsGateway implements OnGatewayConnection {
+  constructor(private readonly authService: AuthService) {}
+
   @WebSocketServer()
   private readonly server: Server;
 
   handleProductUpdated() {
     this.server.emit('productUpdated');
+  }
+
+  handleConnection(client: Socket) {
+    try {
+      this.authService.verifyToken(client.handshake.auth.Authentication.value);
+      console.log('ws auth authorized');
+    } catch (err) {
+      client.disconnect(true);
+      console.log('dégage de là, ws auth pas authorized');
+      //throw new WsException('Unauthorized.');
+    }
   }
 }
